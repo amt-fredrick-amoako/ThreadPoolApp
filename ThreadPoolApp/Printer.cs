e6e6c0c785c3475bcd3868085bbda23592fc83f3
fredrick amoako <fredrickamoako1@gmail.com>
2023-04-27 23:55:25 +0000

    Add project files.

---
+++ ThreadPoolApp/Printer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadPoolApp
+{
+    internal class Printer
+    {
+        private object threadLock = new object();
+        public void PrintNumbers()
+        {
+            Monitor.Enter(threadLock);
+            try
+            {
+                // Display Thread info.
+                Console.WriteLine("-> {0} is executing PrintNumbers()", Thread.CurrentThread.Name);
+
+                // Print out numbers.
+                Console.WriteLine("Your numbers: ");
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.Write("{0} ", i);
+                    Thread.Sleep(2000);
+                }
+                Console.WriteLine();
+            }
+            finally
+            {
+
+                Monitor.Exit(threadLock);
+            }
+        }
+    }
+}
---
+++ ThreadPoolApp/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ThreadPoolApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with the CLR Thread Pool *****\n");
+            Console.WriteLine($"Main thread started. ThreadID = {Thread.CurrentThread.ManagedThreadId}");
+
+            Printer printer = new Printer();
+
+            WaitCallback workItem = PrintTheNumbers;
+
+            // Queue the method ten times.
+            for (int i = 0; i < 10; i++)
+            {
+                ThreadPool.QueueUserWorkItem(workItem, printer);
+            }
+
+            Console.WriteLine("All tasks queued");
+            Console.ReadLine();
+        }
+
+        private static void PrintTheNumbers(object? state)
+        {
+            Printer task = (Printer)state;
+            task.PrintNumbers();
+        }
+    }
+}
